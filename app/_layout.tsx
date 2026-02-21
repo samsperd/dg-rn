6994a15c39c17a05e38fb423dfaed202d95a6a1c
samsperd <iloejeobinna@gmail.com>
2026-02-22 00:29:41 +0100

    Add theme support and configure root layout with SafeAreaProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,31 @@
+import { Colors } from "@/constants/Colors";
 import { Stack } from "expo-router";
+import { Appearance, StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = Appearance.getColorScheme();
+
+  const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
+  const styles = createStyles(theme, colorScheme!);
+
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Todo App" }} />
+      </Stack>
+    </SafeAreaProvider>
+  );
+}
+
+function createStyles(
+  theme: typeof Colors.light | typeof Colors.dark,
+  colorScheme: string,
+) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+  });
 }
